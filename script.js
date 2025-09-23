012df460ea4265d7280cdb53f9918351db2618d7
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2025-09-22 22:55:08 -0300

    Add initial files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var stitchheader = document.getElementById('stitchfofinho')
+ stitchheader.onmouseover = function () {
+    stitchheader.src = "images/gifstitchcoracao.gif"; 
+  }
+
+  stitchheader.onmouseout = function () {
+    stitchheader.src = "images/gifstitchcoracao.png"; 
+  }
+
