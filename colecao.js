5d96439a40e23a96ca85ac4272085fc503ab6f42
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2025-09-30 09:42:37 -0300

    created some pages and editing

---
+++ colecao.js
@@ -0,0 +1,17 @@
+
+
+var stitchheader = document.getElementById('stitchfofinho')
+ stitchheader.onmouseover = function () {
+    stitchheader.src = "images/gifstitchcoracao.gif"; 
+}
+
+  stitchheader.onmouseout = function () {
+    stitchheader.src = "images/gifstitchcoracao.png"; 
+  }
+
+function playVideo() {
+            // Esconde a thumbnail
+    document.querySelector('.thumb').style.opacity = '0'
+    document.querySelector('.thumb').style.display = 'none'
+
+}
--- script.js
+++ script.js
@@ -3,9 +3,25 @@
 var stitchheader = document.getElementById('stitchfofinho')
  stitchheader.onmouseover = function () {
     stitchheader.src = "images/gifstitchcoracao.gif"; 
-  }
+}
 
   stitchheader.onmouseout = function () {
     stitchheader.src = "images/gifstitchcoracao.png"; 
   }
+ //pagina coleção
+function playVideo() {
+            // Esconde a thumbnail
+    document.querySelector('.thumb').style.opacity = '0'
+    document.querySelector('.thumb').style.display = 'none'
 
+}
+
+var fotos = document.getElementsByClassName('fotos');
+
+for (var i = 0; i < fotos.length; i++) {
+  fotos[i].onmouseenter = function () {
+    
+    document.getElementsByClassName('fotosescondidas').style.visibility = 'visible'
+  }
+  
+}
